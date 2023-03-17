01a171cbb50fb6ce155e3ecb8610ec3af388cfaa
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-03-17 18:26:52 +0100

    Añadir la nueva ruta para GET /author

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
 module.exports = router;
