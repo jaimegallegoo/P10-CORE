02330f5bd0f05d5e97d860e5bdd1da1b83d2c67c
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-03-17 18:15:35 +0100

    Corregir errores

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var partials = require('express-partials');    \<--- Añadir
+var partials = require('express-partials');
 
 var indexRouter = require('./routes/index');
 
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(partials());           \<--- Añadir
+app.use(partials());
 
 app.use('/', indexRouter);
 
