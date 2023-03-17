9fb12930ebe52490edba71cd68c124c2bdf9eb02
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-03-17 18:14:18 +0100

    Utilizar express-partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var partials = require('express-partials');    \<--- Añadir
 
 var indexRouter = require('./routes/index');
 
@@ -11,12 +12,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());           \<--- Añadir
 
 app.use('/', indexRouter);
 
