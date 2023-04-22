505851a51b53e4ea8c08240bf309a27b326515c3
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-04-22 19:04:39 +0200

    Definir elementos de la migración de Posts

--- migrations/20230422165350-CreatePostsTable.js
+++ migrations/20230422165350-CreatePostsTable.js
@@ -3,20 +3,49 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'Posts',
+      {
+          id: {
+              type: Sequelize.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              unique: true
+          },
+          title: {
+              type: Sequelize.STRING,
+              validate: {notEmpty: {msg: "title must not be empty."}}
+          },
+          body: {
+              type: Sequelize.TEXT,
+              validate: {notEmpty: {msg: "body must not be empty."}}
+          },
+          attachmentId: {
+            type: Sequelize.STRING,
+            references: {
+              model: "Attachments",
+              key: "id"
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+          },
+          createdAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          },
+          updatedAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          }
+      },
+      {
+          sync: {force: true}
+      }
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Posts');
   }
 };
