332eec9adcf0e7552e7a74461df1c129b8a1eac1
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-04-22 19:01:22 +0200

    Definir elementos de la migración de Attachment

--- migrations/20230422165330-CreateAttachmentsTable.js
+++ migrations/20230422165330-CreateAttachmentsTable.js
@@ -3,20 +3,41 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'Attachments',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          unique: true
+        },
+        mime: {
+          type: Sequelize.STRING
+        },
+        url: {
+          type: Sequelize.STRING
+        },
+        image: {
+          type: Sequelize.BLOB('long')
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      },
+      {
+        sync: {force: true}
+      }
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Attachments');
   }
 };
