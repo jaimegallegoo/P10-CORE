4346359c7929fccf9090d83672ebba6221b144f1
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-04-23 11:14:19 +0200

    Añadir rutas de post y attachment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const postController = require('../controllers/post');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -10,5 +12,11 @@ router.get('/', function(req, res, next) {
 router.get('/author', (req, res, next) => {
   res.render('author');
 });
+
+/* P7 - Tarea 4: load */
+router.param('postId', postController.load);
+
+/* P7 - Tarea 5: GET /posts/:postID/attachment */
+router.get('/posts/:postId(\\d+)/attachment', postController.attachment);
 
 module.exports = router;
