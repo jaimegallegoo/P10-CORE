a71160a0608801d548efe3e2f530fe99728c7277
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-04-26 12:47:10 +0200

    Añadir la ruta de la tarea 6

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,7 @@ router.param('postId', postController.load);
 /* P7 - Tarea 5: GET /posts/:postID/attachment */
 router.get('/posts/:postId(\\d+)/attachment', postController.attachment);
 
+/* P7 - Tarea 6: GET /posts */
+router.get('/posts', postController.index);
+
 module.exports = router;
