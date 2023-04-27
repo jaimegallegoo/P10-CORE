80d8986ffae9425037b4a16c9c80325c2694b195
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-04-27 11:08:20 +0200

    Añadir rutas de la tarea 8

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,19 @@ router.get('/posts', postController.index);
 /* P7 - Tarea 7: GET /posts/:postId */
 router.get('/posts/:postId(\\d+)', postController.show);
 
+/* P7 - Tarea 8.1: GET /posts/new */
+router.get('/posts/new', postController.new);
+
+/* P6 - Tarea 8.2: POST /posts */
+router.post('/posts', upload.single('image'), postController.create);
+
+/* P6 - Tarea 8.3: GET /posts/:postId/edit */
+router.get('/posts/:postId(\\d+)/edit', postController.edit);
+
+/* P6 - Tarea 8.4: GET /posts/:postId/update */
+router.put('/posts/:postId(\\d+)', upload.single('image'), postController.update);
+
+/* P6 - Tarea 8.5: DELETE /posts/:postId */
+router.delete('/posts/:postId(\\d+)', postController.destroy);
+
 module.exports = router;
