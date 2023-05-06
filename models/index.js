0868b6778b6c7de44fbcf5bade5fc70bd2419f0f
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-05-06 21:35:13 +0200

    importar la definición del modelo User

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(url, { logging: false });
 
 const Post = require('./post')(sequelize, Sequelize.DataTypes);
 const Attachment = require('./attachment')(sequelize, Sequelize.DataTypes);
+const User = require('./user')(sequelize, Sequelize.DataTypes);
 
 // Relation 1-to-1 between Post and Attachment
 Attachment.hasOne(Post, {as: 'post', foreignKey: 'attachmentId'});
